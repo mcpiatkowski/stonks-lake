ca509ce810893c86abe939a624c3f52e48b87b34
Michal <mcpiatkowski@gmail.com>
2024-09-15 12:22:16 +0200

    Read the file that triggered the lambda.

--- src/lambda/stonks_raw_function/index.py
+++ src/lambda/stonks_raw_function/index.py
@@ -1,5 +1,23 @@
+import pandas as pd
+import boto3
+from io import StringIO
+
+
 def handler(event, context):
 
+    s3_event = event['Records'][0]['s3']
+    bucket = s3_event['bucket']['name']
+    key = s3_event['object']['key']
+
+    s3 = boto3.client('s3')
+
+    response = s3.get_object(Bucket=bucket, Key=key)
+
+    file_content = response['Body'].read().decode('utf-8')
+
+    df = pd.read_csv(StringIO(file_content))
+
+    print(df.head())
     print(event)
 
     return {
