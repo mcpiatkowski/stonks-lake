0442d1ac238db70993431b42247e52e08e985e6e
Michal <mcpiatkowski@gmail.com>
2024-10-29 19:35:33 +0100

    Successful deployment update.

--- src/app.py
+++ src/app.py
@@ -2,12 +2,21 @@ import aws_cdk as cdk
 import os
 
 from stacks.stonks_lake_stack import StonksLakeStack
+from stacks.stonks_api_stack import StonksApiStack
+
+env = cdk.Environment(account=os.getenv("CDK_DEFAULT_ACCOUNT"), region=os.getenv("CDK_DEFAULT_REGION"))
 
 app = cdk.App()
 StonksLakeStack(
     app,
     construct_id="StonksLakeStack",
-    env=cdk.Environment(account=os.getenv("CDK_DEFAULT_ACCOUNT"), region=os.getenv("CDK_DEFAULT_REGION")),
+    env=env,
+)
+
+StonksApiStack(
+    app,
+    construct_id="StonksApiStack",
+    env=env,
 )
 
 app.synth()
--- src/stacks/stonks_api_stack.py
+++ src/stacks/stonks_api_stack.py
@@ -1,5 +1,8 @@
 from constructs import Construct
 from aws_cdk import (
+    Duration,
+    Aws,
+    RemovalPolicy,
     Stack,
     aws_apigateway as apigateway,
     aws_lambda as lambda_,
@@ -57,9 +60,8 @@ class StonksApiStack(Stack):
 
         api_role = iam.Role(
             self, "ApiGatewayRole",
-            assumed_by=iam.CompositePrincipal(
-                iam.AccountRootPrincipal()
-            )
+            assumed_by=iam.AccountPrincipal(Aws.ACCOUNT_ID),
+            role_name="sf-stonks-api-role"
         )
 
         lambda_fn = lambda_.Function(
@@ -75,18 +77,26 @@ class StonksApiStack(Stack):
             self, "ExternalFunctionApi",
             description="Snowflake external functions Gateway",
             endpoint_types=[apigateway.EndpointType.REGIONAL],
+            deploy=True,
+            deploy_options=apigateway.StageOptions(
+                stage_name="sf-dev"
+            ),
             policy=iam.PolicyDocument(
                 statements=[
                     iam.PolicyStatement(
                         effect=iam.Effect.ALLOW,
                         principals=[iam.ArnPrincipal(
-                            f"arn:aws:sts::{Stack.of(self).account}:assumed-role/ext-func-api-role/snowflake")],
+                            # f"arn:aws:sts::{Stack.of(self).account}:assumed-role/ext-func-api-role/snowflake"
+                            api_role.role_arn
+                        )],
                         actions=["execute-api:Invoke"],
                         resources=["execute-api:/*"]
                     )
                 ]
             )
         )
+
+        api.apply_removal_policy(RemovalPolicy.DESTROY)
 
         echo_resource = api.root.add_resource("echo")
         echo_resource.add_method(
@@ -98,18 +108,6 @@ class StonksApiStack(Stack):
             authorization_type=apigateway.AuthorizationType.IAM
         )
 
-        deployment = apigateway.Deployment(
-            self, "Deployment",
-            api=api
-        )
-
-        stage = apigateway.Stage(
-            self, "Stage",
-            deployment=deployment
-        )
-
-        api.deployment_stage = stage
-
         lambda_fn.add_permission(
             "ApiGatewayInvoke",
             principal=iam.ServicePrincipal("apigateway.amazonaws.com"),
@@ -119,7 +117,7 @@ class StonksApiStack(Stack):
 
         CfnOutput(
             self, "ResourceInvocationUrl",
-            value=f"https://{api.rest_api_id}.execute-api.{Stack.of(self).region}.amazonaws.com/ext-func-stage/echo"
+            value=f"https://{api.rest_api_id}.execute-api.{Stack.of(self).region}.amazonaws.com/sf-dev/echo"
         )
 
         CfnOutput(
